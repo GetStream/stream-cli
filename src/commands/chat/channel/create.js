60977cf682594fd4ce81405157559663f303f9e1
Nick Parsons <nparsons08@gmail.com>
2019-03-27 14:34:00 -0600

    WIP

--- src/commands/chat/channel/create.js
+++ src/commands/chat/channel/create.js
@@ -7,122 +7,122 @@ const { auth } = require('../../../utils/auth');
 const { credentials } = require('../../../utils/config');
 
 class ChannelCreate extends Command {
-	async run() {
-		const { flags } = this.parse(ChannelCreate);
+    async run() {
+        const { flags } = this.parse(ChannelCreate);
 
-		try {
-			if (!flags.json) {
-				const res = await prompt([
-					{
-						type: 'input',
-						name: 'channel',
-						message: `What is the unique identifier for the channel?`,
-						required: true,
-					},
-					{
-						type: 'select',
-						name: 'type',
-						message: 'What type of channel is this?',
-						required: true,
-						choices: [
-							{ message: 'Livestream', value: 'livestream' },
-							{ message: 'Messaging', value: 'messaging' },
-							{ message: 'Gaming', value: 'gaming' },
-							{ message: 'Commerce', value: 'commerce' },
-							{ message: 'Team', value: 'team' },
-						],
-					},
-					{
-						type: 'input',
-						name: 'image',
-						message: `What is the absolute URL to the channel image?`,
-						required: true,
-					},
-				]);
+        try {
+            if (!flags.channel || !flags.type || !flags.image) {
+                const res = await prompt([
+                    {
+                        type: 'input',
+                        name: 'channel',
+                        message: `What is the unique identifier for the channel?`,
+                        required: true,
+                    },
+                    {
+                        type: 'select',
+                        name: 'type',
+                        message: 'What type of channel is this?',
+                        required: true,
+                        choices: [
+                            { message: 'Livestream', value: 'livestream' },
+                            { message: 'Messaging', value: 'messaging' },
+                            { message: 'Gaming', value: 'gaming' },
+                            { message: 'Commerce', value: 'commerce' },
+                            { message: 'Team', value: 'team' },
+                        ],
+                    },
+                    {
+                        type: 'input',
+                        name: 'image',
+                        message: `What is the absolute URL to the channel image?`,
+                        required: false,
+                    },
+                ]);
 
-				for (const key in res) {
-					if (res.hasOwnProperty(key)) {
-						flags[key] = res[key];
-					}
-				}
-			}
+                for (const key in res) {
+                    if (res.hasOwnProperty(key)) {
+                        flags[key] = res[key];
+                    }
+                }
+            }
 
-			const { name } = await credentials(this);
-			const client = await auth(this);
+            const { name } = await credentials(this);
+            const client = await auth(this);
 
-			let payload = {
-				name: flags.name,
-				created_by: {
-					id: 'CLI',
-					name,
-				},
-			};
-			if (flags.image) payload.image = flags.image;
+            let payload = {
+                name: flags.name,
+                created_by: {
+                    id: 'CLI',
+                    name,
+                },
+            };
+            if (flags.image) payload.image = flags.image;
 
-			if (flags.data) {
-				const parsed = JSON.parse(flags.data);
-				payload = Object.assign({}, payload, parsed);
-			}
+            if (flags.data) {
+                const parsed = JSON.parse(flags.data);
+                payload = Object.assign({}, payload, parsed);
+            }
 
-			const channel = await client.channel(
-				flags.type,
-				flags.channel,
-				payload
-			);
+            const channel = await client.channel(
+                flags.type,
+                flags.channel,
+                payload
+            );
 
-			const create = await channel.create();
+            const create = await channel.create();
 
-			if (flags.json) {
-				this.log(JSON.stringify(create.channel));
-				this.exit();
-			}
+            if (flags.json) {
+                this.log(JSON.stringify(create.channel));
+                this.exit();
+            }
 
-			this.log(
-				`Channel ${chalk.bold(create.channel.id)} has been created.`
-			);
-			this.exit();
-		} catch (error) {
-			this.error(error || 'A Stream CLI error has occurred.', {
-				exit: 1,
-			});
-		}
-	}
+            this.log(
+                `Channel ${chalk.bold(create.channel.id)} has been created.`
+            );
+            this.exit();
+        } catch (error) {
+            this.error(error || 'A Stream CLI error has occurred.', {
+                exit: 1,
+            });
+        }
+    }
 }
 
 ChannelCreate.flags = {
-	channel: flags.string({
-		char: 'c',
-		description: 'A unique ID for the channel you wish to create.',
-		default: uuid(),
-		required: false,
-	}),
-	type: flags.string({
-		char: 't',
-		description: 'Type of channel.',
-		options: ['livestream', 'messaging', 'gaming', 'commerce', 'team'],
-		required: false,
-	}),
-	name: flags.string({
-		char: 'n',
-		description: 'Name of the channel room.',
-		required: false,
-	}),
-	image: flags.string({
-		char: 'i',
-		description: 'URL to channel image.',
-		required: false,
-	}),
-	data: flags.string({
-		char: 'd',
-		description: 'Additional data as JSON.',
-		required: false,
-	}),
-	json: flags.boolean({
-		char: 'j',
-		description:
-			'Output results in JSON. When not specified, returns output in a human friendly format.',
-		required: false,
-	}),
+    channel: flags.string({
+        char: 'c',
+        description: 'A unique ID for the channel you wish to create.',
+        default: uuid(),
+        required: false,
+    }),
+    type: flags.string({
+        char: 't',
+        description: 'Type of channel.',
+        options: ['livestream', 'messaging', 'gaming', 'commerce', 'team'],
+        required: false,
+    }),
+    name: flags.string({
+        char: 'n',
+        description: 'Name of the channel room.',
+        required: false,
+    }),
+    image: flags.string({
+        char: 'i',
+        description: 'URL to channel image.',
+        required: false,
+    }),
+    data: flags.string({
+        char: 'd',
+        description: 'Additional data as JSON.',
+        required: false,
+    }),
+    json: flags.boolean({
+        char: 'j',
+        description:
+            'Output results in JSON. When not specified, returns output in a human friendly format.',
+        required: false,
+    }),
 };
 
 module.exports.ChannelCreate = ChannelCreate;
--- src/commands/chat/channel/query.js
+++ src/commands/chat/channel/query.js
@@ -3,62 +3,63 @@ const { Command, flags } = require('@oclif/command');
 const { auth } = require('../../../utils/auth');
 
 class ChannelQuery extends Command {
-	async run() {
-		const { flags } = this.parse(ChannelQuery);
+    async run() {
+        const { flags } = this.parse(ChannelQuery);
 
-		try {
-			const client = await auth(this);
+        try {
+            const client = await auth(this);
 
-			const filter = flags.filters ? JSON.parse(flags.filters) : {};
-			const sort = flags.sort ? JSON.parse(flags.sort) : {};
+            const filter = flags.filters ? JSON.parse(flags.filters) : {};
+            const sort = flags.sort ? JSON.parse(flags.sort) : {};
 
-			const channel = await client.queryChannels(filter, sort, {
-				subscribe: false,
-			});
+            const channel = await client.queryChannels(filter, sort, {
+                subscribe: false,
+            });
 
-			if (flags.json) {
-				this.log(JSON.stringify(channel[0].data));
-				this.exit();
-			}
+            if (flags.json) {
+                this.log(JSON.stringify(channel[0].data));
+                this.exit();
+            }
 
-			this.log(channel[0].data);
-			this.exit();
-		} catch (error) {
-			this.error(error || 'A Stream CLI error has occurred.', {
-				exit: 1,
-			});
-		}
-	}
+            this.log(channel[0].data);
+            this.exit();
+        } catch (error) {
+            this.error(error || 'A Stream CLI error has occurred.', {
+                exit: 1,
+            });
+        }
+    }
 }
 
 ChannelQuery.flags = {
-	channel: flags.string({
-		char: 'c',
-		description: 'The channel ID you want to query.',
-		required: false,
-	}),
-	type: flags.string({
-		char: 't',
-		description: 'Type of channel.',
-		options: ['livestream', 'messaging', 'gaming', 'commerce', 'team'],
-		required: false,
-	}),
-	filter: flags.string({
-		char: 'f',
-		description: 'Filters to apply to the query.',
-		required: false,
-	}),
-	sort: flags.string({
-		char: 's',
-		description: 'Sort to apply to the query.',
-		required: false,
-	}),
-	json: flags.boolean({
-		char: 'j',
-		description:
-			'Output results in JSON. When not specified, returns output in a human friendly format.',
-		required: false,
-	}),
+    channel: flags.string({
+        char: 'c',
+        description:
+            'The unique indentifier for the channel you want to query.',
+        required: false,
+    }),
+    type: flags.string({
+        char: 't',
+        description: 'Type of channel.',
+        options: ['livestream', 'messaging', 'gaming', 'commerce', 'team'],
+        required: false,
+    }),
+    filter: flags.string({
+        char: 'f',
+        description: 'Filters to apply to the query.',
+        required: false,
+    }),
+    sort: flags.string({
+        char: 's',
+        description: 'Sort to apply to the query.',
+        required: false,
+    }),
+    json: flags.boolean({
+        char: 'j',
+        description:
+            'Output results in JSON. When not specified, returns output in a human friendly format.',
+        required: false,
+    }),
 };
 
 module.exports.ChannelQuery = ChannelQuery;
