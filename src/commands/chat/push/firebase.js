18a9d3090471f4f911687c61c9bdb33b72db7a7c
Nick Parsons <nparsons08@gmail.com>
2019-03-27 10:37:20 -0600

    Removed set command for push

---
+++ src/commands/chat/push/firebase.js
@@ -0,0 +1,94 @@
+const { Command, flags } = require('@oclif/command');
+const { prompt } = require('enquirer');
+const chalk = require('chalk');
+
+const { auth } = require('../../../utils/auth');
+
+class PushFirebase extends Command {
+    async run() {
+        const { flags } = this.parse(PushFirebase);
+
+        try {
+            if (!flags.key || !flags.notification_template) {
+                const res = await prompt([
+                    {
+                        type: 'input',
+                        name: 'key',
+                        message: `What is your API key for Firebase?`,
+                        required: true,
+                    },
+                    {
+                        type: 'input',
+                        name: 'notification_template',
+                        hint: 'Omit for Stream default',
+                        message: `What JSON notification template would you like to use?`,
+                        required: false,
+                    },
+                ]);
+
+                for (const key in res) {
+                    if (res.hasOwnProperty(key)) {
+                        flags[key] = res[key];
+                    }
+                }
+            }
+
+            const client = await auth(this);
+
+            const payload = {
+                firebase_config: {
+                    api_key: flags.key,
+                    notification_template: flags.notification_template || '',
+                },
+            };
+
+            if (flags.notification_template) {
+                payload.firebase_config.notification_template =
+                    flags.notification_template;
+            }
+
+            await client.updateAppSettings(payload);
+
+            if (flags.json) {
+                const settings = await client.getAppSettings();
+
+                this.log(
+                    JSON.stringify(settings.app.push_notifications.firebase)
+                );
+                this.exit();
+            }
+
+            this.log(
+                `Push notifications have been enabled for ${chalk.bold(
+                    'Firebase'
+                )}.`
+            );
+            this.exit(0);
+        } catch (error) {
+            this.error(error || 'A Stream CLI error has occurred.', {
+                exit: 1,
+            });
+        }
+    }
+}
+
+PushFirebase.flags = {
+    key: flags.string({
+        char: 'k',
+        description: 'API key for Firebase.',
+        required: false,
+    }),
+    notification_template: flags.string({
+        char: 'n',
+        description: 'JSON notification template.',
+        required: false,
+    }),
+    json: flags.boolean({
+        char: 'j',
+        description:
+            'Output results in JSON. When not specified, returns output in a human friendly format.',
+        required: false,
+    }),
+};
+
+module.exports.PushFirebase = PushFirebase;
--- src/commands/chat/reaction/create.js
+++ src/commands/chat/reaction/create.js
@@ -4,103 +4,105 @@ const { prompt } = require('enquirer');
 const { auth } = require('../../../utils/auth');
 
 class ReactionCreate extends Command {
-	async run() {
-		const { flags } = this.parse(ReactionCreate);
+    async run() {
+        const { flags } = this.parse(ReactionCreate);
 
-		try {
-			if (!flags.json) {
-				const res = await prompt([
-					{
-						type: 'input',
-						name: 'channel',
-						message: `What is the unique identifier for the channel?`,
-						required: true,
-					},
-					{
-						type: 'select',
-						name: 'type',
-						message: 'What type of channel is this?',
-						required: true,
-						choices: [
-							{ message: 'Livestream', value: 'livestream' },
-							{ message: 'Messaging', value: 'messaging' },
-							{ message: 'Gaming', value: 'gaming' },
-							{ message: 'Commerce', value: 'commerce' },
-							{ message: 'Team', value: 'team' },
-						],
-					},
-					{
-						type: 'input',
-						name: 'message',
-						message: `What is the unique identifier for the message?`,
-						required: true,
-					},
-					{
-						type: 'input',
-						name: 'reaction',
-						hint: 'love',
-						message: 'What is the reaction you would like to add?',
-						required: true,
-					},
-				]);
+        try {
+            if (!flags.json) {
+                const res = await prompt([
+                    {
+                        type: 'input',
+                        name: 'channel',
+                        message:
+                            'What is the unique identifier for the channel?',
+                        required: true,
+                    },
+                    {
+                        type: 'select',
+                        name: 'type',
+                        message: 'What type of channel is this?',
+                        required: true,
+                        choices: [
+                            { message: 'Livestream', value: 'livestream' },
+                            { message: 'Messaging', value: 'messaging' },
+                            { message: 'Gaming', value: 'gaming' },
+                            { message: 'Commerce', value: 'commerce' },
+                            { message: 'Team', value: 'team' },
+                        ],
+                    },
+                    {
+                        type: 'input',
+                        name: 'message',
+                        message:
+                            'What is the unique identifier for the message?',
+                        required: true,
+                    },
+                    {
+                        type: 'input',
+                        name: 'reaction',
+                        hint: 'love',
+                        message: 'What is the reaction you would like to add?',
+                        required: true,
+                    },
+                ]);
 
-				for (const key in res) {
-					if (res.hasOwnProperty(key)) {
-						flags[key] = res[key];
-					}
-				}
-			}
+                for (const key in res) {
+                    if (res.hasOwnProperty(key)) {
+                        flags[key] = res[key];
+                    }
+                }
+            }
 
-			const client = await auth(this);
+            const client = await auth(this);
 
-			const channel = client.channel(flags.type, flags.channel);
-			const reaction = await channel.sendReaction(flags.message, {
-				type: flags.reaction,
-			});
+            const channel = client.channel(flags.type, flags.channel);
+            const reaction = await channel.sendReaction(flags.message, {
+                type: flags.reaction,
+            });
 
-			if (this.json) {
-				this.log(JSON.stringify(reaction));
-				this.exit(0);
-			}
+            if (this.json) {
+                this.log(JSON.stringify(reaction));
+                this.exit(0);
+            }
 
-			this.log('Your reaction has been created.');
-			this.exit(0);
-		} catch (error) {
-			this.error(error || 'A Stream CLI error has occurred.', {
-				exit: 1,
-			});
-		}
-	}
+            this.log('Your reaction has been created.');
+            this.exit(0);
+        } catch (error) {
+            this.error(error || 'A Stream CLI error has occurred.', {
+                exit: 1,
+            });
+        }
+    }
 }
 
 ReactionCreate.flags = {
-	channel: flags.string({
-		char: 'c',
-		description: 'The unique identifier for the channel.',
-		required: false,
-	}),
-	type: flags.string({
-		char: 't',
-		description: 'The type of channel.',
-		options: ['livestream', 'messaging', 'gaming', 'commerce', 'team'],
-		required: false,
-	}),
-	message: flags.string({
-		char: 'c',
-		description: 'The unique identifier for the message.',
-		required: false,
-	}),
-	reaction: flags.string({
-		char: 'r',
-		description: 'A reaction for the message (e.g. love).',
-		required: false,
-	}),
-	json: flags.boolean({
-		char: 'j',
-		description:
-			'Output results in JSON. When not specified, returns output in a human friendly format.',
-		required: false,
-	}),
+    channel: flags.string({
+        char: 'c',
+        description: 'The unique identifier for the channel.',
+        required: false,
+    }),
+    type: flags.string({
+        char: 't',
+        description: 'The type of channel.',
+        options: ['livestream', 'messaging', 'gaming', 'commerce', 'team'],
+        required: false,
+    }),
+    message: flags.string({
+        char: 'c',
+        description: 'The unique identifier for the message.',
+        required: false,
+    }),
+    reaction: flags.string({
+        char: 'r',
+        description: 'A reaction for the message (e.g. love).',
+        required: false,
+    }),
+    json: flags.boolean({
+        char: 'j',
+        description:
+            'Output results in JSON. When not specified, returns output in a human friendly format.',
+        required: false,
+    }),
 };
 
 module.exports.ReactionCreate = ReactionCreate;
