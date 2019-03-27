a92d8d7f62302e5847613a7a26314b2cb824b0e6
Nick Parsons <nparsons08@gmail.com>
2019-03-27 10:39:08 -0600

    WIP

--- src/commands/chat/push/webhook.js
+++ src/commands/chat/push/webhook.js
@@ -5,65 +5,65 @@ const chalk = require('chalk');
 const { auth } = require('../../../utils/auth');
 
 class PushWebhook extends Command {
-    async run() {
-        const { flags } = this.parse(PushWebhook);
+	async run() {
+		const { flags } = this.parse(PushWebhook);
 
-        try {
-            if (!flags.url) {
-                const res = await prompt([
-                    {
-                        type: 'input',
-                        name: 'url',
-                        message: `What is the absolute URL for your webhook?`,
-                        required: true,
-                    },
-                ]);
+		try {
+			if (!flags.url) {
+				const res = await prompt([
+					{
+						type: 'input',
+						name: 'url',
+						message: `What is the absolute URL for your webhook?`,
+						required: true,
+					},
+				]);
 
-                flags.url = res.url;
-            }
+				flags.url = res.url;
+			}
 
-            const client = await auth(this);
-            await client.updateAppSettings({
-                webhook_url: flags.url,
-            });
+			const client = await auth(this);
+			await client.updateAppSettings({
+				webhook_url: flags.url,
+			});
 
-            if (flags.json) {
-                const settings = await client.getAppSettings();
+			if (flags.json) {
+				const settings = await client.getAppSettings();
 
-                this.log(
-                    JSON.stringify({
-                        webhook_url: settings.app.webhook_url,
-                    })
-                );
-                this.exit();
-            }
+				this.log(
+					JSON.stringify({
+						webhook_url: settings.app.webhook_url,
+					})
+				);
+				this.exit();
+			}
 
-            this.log(
-                `Push notifications have been enabled for ${chalk.bold(
-                    'Webhooks'
-                )}.`
-            );
-            this.exit(0);
-        } catch (error) {
-            this.error(error || 'A Stream CLI error has occurred.', {
-                exit: 1,
-            });
-        }
-    }
+			this.log(
+				`Push notifications have been enabled for ${chalk.bold(
+					'Webhooks'
+				)}.`
+			);
+			this.exit(0);
+		} catch (error) {
+			this.error(error || 'A Stream CLI error has occurred.', {
+				exit: 1,
+			});
+		}
+	}
 }
 
 PushWebhook.flags = {
-    url: flags.string({
-        char: 'u',
-        description: 'Fully qualified URL for webhook support.',
-        required: false,
-    }),
-    json: flags.boolean({
-        char: 'j',
-        description:
-            'Output results in JSON. When not specified, returns output in a human friendly format.',
-        required: false,
-    }),
+	url: flags.string({
+		char: 'u',
+		description: 'Fully qualified URL for webhook support.',
+		required: false,
+	}),
+	json: flags.boolean({
+		char: 'j',
+		description:
+			'Output results in JSON. When not specified, returns output in a human friendly format.',
+		required: false,
+	}),
 };
 
 module.exports.PushWebhook = PushWebhook;
