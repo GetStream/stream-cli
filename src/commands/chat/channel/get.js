b069c48499cb428ec8b1ae8fb586791e1f66f412
Nick Parsons <nparsons08@gmail.com>
2019-06-03 11:27:17 -0600

    Hotfix

--- src/commands/chat/channel/get.js
+++ src/commands/chat/channel/get.js
@@ -4,72 +4,72 @@ const { prompt } = require('enquirer');
 const { chatAuth } = require('../../../utils/auth/chat-auth');
 
 class ChannelGet extends Command {
-    async run() {
-        const { flags } = this.parse(ChannelGet);
+	async run() {
+		const { flags } = this.parse(ChannelGet);
 
-        try {
-            if (!flags.channel || !flags.type) {
-                const res = await prompt([
-                    {
-                        type: 'input',
-                        name: 'channel',
-                        message: `What is the unique identifier for the channel?`,
-                        required: true,
-                    },
-                    {
-                        type: 'select',
-                        name: 'type',
-                        message: 'What type of channel is this?',
-                        required: true,
-                        choices: [
-                            { message: 'Livestream', value: 'livestream' },
-                            { message: 'Messaging', value: 'messaging' },
-                            { message: 'Gaming', value: 'gaming' },
-                            { message: 'Commerce', value: 'commerce' },
-                            { message: 'Team', value: 'team' },
-                        ],
-                    },
-                ]);
+		try {
+			if (!flags.channel || !flags.type) {
+				const res = await prompt([
+					{
+						type: 'input',
+						name: 'channel',
+						message: `What is the unique identifier for the channel?`,
+						required: true,
+					},
+					{
+						type: 'select',
+						name: 'type',
+						message: 'What type of channel is this?',
+						required: true,
+						choices: [
+							{ message: 'Livestream', value: 'livestream' },
+							{ message: 'Messaging', value: 'messaging' },
+							{ message: 'Gaming', value: 'gaming' },
+							{ message: 'Commerce', value: 'commerce' },
+							{ message: 'Team', value: 'team' },
+						],
+					},
+				]);
 
-                for (const key in res) {
-                    if (res.hasOwnProperty(key)) {
-                        flags[key] = res[key];
-                    }
-                }
-            }
+				for (const key in res) {
+					if (res.hasOwnProperty(key)) {
+						flags[key] = res[key];
+					}
+				}
+			}
 
-            const client = await chatAuth(this);
+			const client = await chatAuth(this);
 
-            const channel = await client.queryChannels(
-                { id: flags.channel, type: flags.type },
-                { last_message_at: -1 },
-                {
-                    subscribe: false,
-                }
-            );
+			const channel = await client.queryChannels(
+				{ id: flags.channel, type: flags.type },
+				{ last_message_at: -1 },
+				{
+					subscribe: false,
+				}
+			);
 
-            this.log(JSON.stringify(channel[0].data));
-            this.exit();
-        } catch (error) {
-            this.error(error || 'A Stream CLI error has occurred.', {
-                exit: 1,
-            });
-        }
-    }
+			this.log(JSON.stringify(channel[0].data));
+			this.exit();
+		} catch (error) {
+			this.error(error || 'A Stream CLI error has occurred.', {
+				exit: 1,
+			});
+		}
+	}
 }
 
 ChannelGet.flags = {
-    channel: flags.string({
-        char: 'c',
-        description: 'The channel ID you wish to retrieve.',
-        required: false,
-    }),
-    type: flags.string({
-        char: 't',
-        description: 'Type of channel.',
-        options: ['livestream', 'messaging', 'gaming', 'commerce', 'team'],
-        required: false,
-    }),
+	channel: flags.string({
+		char: 'c',
+		description: 'The channel ID you wish to retrieve.',
+		required: false,
+	}),
+	type: flags.string({
+		char: 't',
+		description: 'Type of channel.',
+		options: ['livestream', 'messaging', 'gaming', 'commerce', 'team'],
+		required: false,
+	}),
 };
 
 module.exports.ChannelGet = ChannelGet;
