25b8655c3b60cbeb0ce967bdd2f8e3ea5d3819ec
peterdeme <snypox@gmail.com>
2022-03-07 14:47:06 +0100

    feat: add channel type crud

--- pkg/cmd/chat/root.go
+++ pkg/cmd/chat/root.go
@@ -3,6 +3,7 @@ package chat
 import (
 	"github.com/GetStream/stream-cli/pkg/cmd/app"
 	"github.com/GetStream/stream-cli/pkg/cmd/chat/channel"
+	"github.com/GetStream/stream-cli/pkg/cmd/chat/channeltype"
 	"github.com/GetStream/stream-cli/pkg/cmd/chat/watch"
 	"github.com/spf13/cobra"
 )
@@ -13,8 +14,9 @@ func NewRootCmd() *cobra.Command {
 		Short: "Interact with your Stream Chat application",
 	}
 
-	cmd.AddCommand(channel.NewCmds()...)
 	cmd.AddCommand(app.NewCmds()...)
+	cmd.AddCommand(channel.NewCmds()...)
+	cmd.AddCommand(channeltype.NewCmds()...)
 	cmd.AddCommand(watch.NewCmds()...)
 
 	return cmd
