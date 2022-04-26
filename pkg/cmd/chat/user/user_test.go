694443263a760bec308a4147aa615b7644e1ce72
Peter Deme <snypox@gmail.com>
2022-04-26 13:27:41 +0200

    feat(user): add missing commands for users (#97)

--- pkg/cmd/chat/user/user_test.go
+++ pkg/cmd/chat/user/user_test.go
@@ -68,3 +68,76 @@ func TestRevokeToken(t *testing.T) {
 	require.NoError(t, err)
 	require.Contains(t, cmd.OutOrStdout().(*bytes.Buffer).String(), "Successfully revoked token")
 }
+
+func TestBanUnbanUser(t *testing.T) {
+	cmd := test.GetRootCmdWithSubCommands(NewCmds()...)
+	userToBan := test.CreateUser()
+	bannerUser := test.CreateUser()
+	t.Cleanup(func() {
+		test.DeleteUser(userToBan)
+		test.DeleteUser(bannerUser)
+	})
+
+	cmd.SetArgs([]string{"ban-user", "-t", userToBan, "-b", bannerUser, "--reason", "test"})
+	_, err := cmd.ExecuteC()
+	require.NoError(t, err)
+	require.Contains(t, cmd.OutOrStdout().(*bytes.Buffer).String(), "Successfully banned user")
+
+	cmd.SetArgs([]string{"unban-user", "-t", userToBan})
+	_, err = cmd.ExecuteC()
+	require.NoError(t, err)
+	require.Contains(t, cmd.OutOrStdout().(*bytes.Buffer).String(), "Successfully unbanned user")
+}
+
+func TestDeactivateReactivate(t *testing.T) {
+	cmd := test.GetRootCmdWithSubCommands(NewCmds()...)
+	userToDeactivate := test.CreateUser()
+	t.Cleanup(func() {
+		test.DeleteUser(userToDeactivate)
+	})
+
+	cmd.SetArgs([]string{"deactivate-user", "-u", userToDeactivate})
+	_, err := cmd.ExecuteC()
+	require.NoError(t, err)
+	require.Contains(t, cmd.OutOrStdout().(*bytes.Buffer).String(), "Successfully deactivated user")
+
+	cmd.SetArgs([]string{"reactivate-user", "-u", userToDeactivate})
+	_, err = cmd.ExecuteC()
+	require.NoError(t, err)
+	require.Contains(t, cmd.OutOrStdout().(*bytes.Buffer).String(), "Successfully reactivated user")
+}
+
+func TestMuteUnmute(t *testing.T) {
+	cmd := test.GetRootCmdWithSubCommands(NewCmds()...)
+	userToMute := test.CreateUser()
+	muterUser := test.CreateUser()
+	t.Cleanup(func() {
+		test.DeleteUser(userToMute)
+		test.DeleteUser(muterUser)
+	})
+
+	cmd.SetArgs([]string{"mute-user", "-t", userToMute, "-b", muterUser, "--expiration", "5"})
+	_, err := cmd.ExecuteC()
+	require.NoError(t, err)
+	require.Contains(t, cmd.OutOrStdout().(*bytes.Buffer).String(), "Successfully muted user")
+
+	cmd.SetArgs([]string{"unmute-user", "-t", userToMute, "-b", muterUser})
+	_, err = cmd.ExecuteC()
+	require.NoError(t, err)
+	require.Contains(t, cmd.OutOrStdout().(*bytes.Buffer).String(), "Successfully unmuted user")
+}
+
+func TestFlag(t *testing.T) {
+	cmd := test.GetRootCmdWithSubCommands(NewCmds()...)
+	userToFlag := test.CreateUser()
+	flaggerUser := test.CreateUser()
+	t.Cleanup(func() {
+		test.DeleteUser(userToFlag)
+		test.DeleteUser(flaggerUser)
+	})
+
+	cmd.SetArgs([]string{"flag-user", "-u", userToFlag, "-b", flaggerUser})
+	_, err := cmd.ExecuteC()
+	require.NoError(t, err)
+	require.Contains(t, cmd.OutOrStdout().(*bytes.Buffer).String(), "Successfully flagged user")
+}
